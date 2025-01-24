3b8878187d0357319968556ae1d5abe84c012ebd
PhilGustafsson <philipgustafsson00@gmail.com>
2025-01-24 13:17:55 +0100

    ljutljanka submission accepted

---
+++ Exercises/exercise_1/ljutljanka/target/debug/deps/ljutljanka-0847fcad6c26191c.d
@@ -0,0 +1,5 @@
+/Users/philip/Public/University/Algorithmic-Problem-Solving/Exercises/exercise_1/ljutljanka/target/debug/deps/ljutljanka-0847fcad6c26191c: src/main.rs
+
+/Users/philip/Public/University/Algorithmic-Problem-Solving/Exercises/exercise_1/ljutljanka/target/debug/deps/ljutljanka-0847fcad6c26191c.d: src/main.rs
+
+src/main.rs:
---
+++ Exercises/exercise_1/ljutljanka/target/debug/deps/ljutljanka-6055b2360fe583ee.d
@@ -0,0 +1,5 @@
+/Users/philip/Public/University/Algorithmic-Problem-Solving/Exercises/ljutljanka/target/debug/deps/libljutljanka-6055b2360fe583ee.rmeta: src/main.rs
+
+/Users/philip/Public/University/Algorithmic-Problem-Solving/Exercises/ljutljanka/target/debug/deps/ljutljanka-6055b2360fe583ee.d: src/main.rs
+
+src/main.rs:
